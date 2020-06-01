ef79e6a72a50b8bf7999338f3fda194fc2085a52
Kestas Blazulionis <kestas.blazulionis@gmail.com>
2020-06-01 14:03:55 +0300

    initial version

--- app/Jobs/DatadogWebhookJob.php
+++ app/Jobs/DatadogWebhookJob.php
@@ -13,6 +13,24 @@ class DatadogWebhookJob extends SpatieProcessWebhookJob
         $this->webhookCall; // contains an instance of `WebhookCall`
 
         $payload = json_decode($this->webhookCall, true);
-        Log::alert($payload);
+
+        $alert['severity'] = $payload['payload']['alert_type'];
+        $alert['tags'] = [];
+
+        foreach(explode(',',$payload['payload']['tags']) as $tag)
+        {
+            $alert['tags'][explode(':',$tag)[0]] = explode(':',$tag)[1] ?? null;
+        }
+        $alert['title'] = preg_replace(
+            array('/(.+}])/'),
+            array(''),
+            $payload['payload']['title']
+        );
+        $alert['object'] = $alert['tags']['host'] ?? $payload['payload']['hostname'];
+
+        $alert['node'] = $alert['tags']['project'].'_'.$alert['tags']['system'].'-appid-'.$alert['tags']['application_id'];
+        $alert['category'] = $alert['tags']['service_owner_support'];
+
+        Log::alert($alert);
     }
 }
